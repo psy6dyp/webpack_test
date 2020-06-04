20a41aa4e90ae647d2007185768ccc20abb5605a
psy6dyp <psy6dyp@163.com>
2020-06-04 11:16:04 +0800

    css的兼容性处理方案

---
+++ webpack_08_css兼容性处理/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'js/built.js',
+        path: __dirname + '/build'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    // 需要在package.json中配置browserslist，让postcss知道做哪些兼容性
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            // 注意，此处是返回数组
+                            plugins: () => [
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/built.css'
+        })
+    ],
+    mode: 'development'
+}
